749a93953f39fc1c99e4f06d79bf423db99e1666
JoseRDGZ <josecrodriguez2004@gmail.com>
2022-03-13 17:14:39 -0400

    added random

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
 from user import UserData
+import random as rnd
 
 users = UserData()
-## users.printAllInfo()
-users.getUser("2271")
+#users.printAllInfo()
+#users.getUser("2271")
+print(rnd.choice(list(users.allInfo.values())))
