5e43ad543fc91af7c518a711a0dd512ec6c33c10
JoseRDGZ <josecrodriguez2004@gmail.com>
2022-03-13 15:55:13 -0400

    work in-progress

--- user.py
+++ user.py
@@ -1,14 +1,18 @@
 import json
 class UserData:
+    allInfo = {}
     def __init__(self, firstName, emailAddress, idNum, lastName):
         self.firstName = firstName 
         self.emailAddress = emailAddress 
         self.idNum = idNum 
         self.lastName = lastName
-        self.allInfo = {}
+        self.createAllInfo()
    
     def createAllInfo(self):
         self.allInfo = json.loads(open('data.json').read())
 
     def printAllInfo(self):
         print(json.dumps(self.allInfo, indent = 4, sort_keys=True))
+
+    def addUser(self, firstName, lastName, idNum,  emailAddress):
+        
